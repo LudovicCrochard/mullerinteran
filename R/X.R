d574f9214620508de72d4caa5fc2e8a21bf3bce7
LudovicCrochard <ludovic.crochard@edu.mnhn.fr>
2019-12-06 15:26:48 +0100

    Remove data compo_new

--- R/X.R
+++ R/X.R
@@ -1,10 +1,13 @@
 #' @title Random dataset to run muller function
-#' 
+#'
 #' @format A dataframe with 4 lines and 6 columns
 #' \describe{
 #'   \item{Year}{years}
 #'   \item{Culture}{random words figuring crop names}
-#'   \item{sit}{this column and the others figure the realtive abundance of virtual species}
+#'   \item{Sit_praesent}{this column figure the realtive abundance of virtual species}
+#'   \item{Amet_phasellus}{this columnfigure the realtive abundance of virtual species}
+#'   \item{Habitasse_ante}{this column figure the realtive abundance of virtual species}
+#'   \item{Est_malesuada}{this column figure the realtive abundance of virtual species}
 #' }
 #' @source Random Number Generator (RNG): 1053
 #'
--- inst/data-raw/compo_new.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `compo_new` dataset goes here
-
-usethis::use_data("compo_new")
